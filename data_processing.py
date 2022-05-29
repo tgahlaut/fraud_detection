9b5be140e3b313b9afd16868396b8313703c9cbc
tgahlaut <tgahlaut@gmail.com>
2022-05-29 16:21:38 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -6,8 +6,10 @@ print("adding all the data processing code related fraud detection")
 
 a=5
 b=7
+
 def sum(a,b):
-    c=a+b
-    return b
-    print("adding a new function sum which is not correct")
-    
+c=(a+b)
+return a
+print("added a new function sum which is not correct")
+
+
