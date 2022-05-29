9d4fe5072f30600c9b6c4aaaad8289ece1a8c186
tgahlaut <tgahlaut@gmail.com>
2022-05-29 09:04:45 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,14 @@
 
+from re import A
+
+
 print("adding all the data processing code related fraud detection")
+
+a=5
+b=7
+
+def sum(a,b):
+c=(a+b)
+return a
+print("added a new function sum which is not correct")
+
