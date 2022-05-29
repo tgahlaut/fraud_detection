2cc0ed040da87039cde6a6001d4ac3657ca30a79
tgahlaut <tgahlaut@gmail.com>
2022-05-29 20:21:53 +0530

    adding data processung python code

--- data_processing.py
+++ data_processing.py
@@ -1,15 +1,2 @@
 
-from re import A
-
-
 print("adding all the data processing code related fraud detection")
-
-a=5
-b=7
-
-def sum(a,b):
-   c=(a+b)
-return a
-print("added a new function sum")
-
-
